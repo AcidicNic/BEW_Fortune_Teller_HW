c9ba0ba114c8522a4d1d6b6c7360f0a5e52295bc
snekek <nic4096@gmail.com>
2019-10-26 19:46:07 -0700

    hw2 finished :)

--- app.py
+++ app.py
@@ -1,19 +1,45 @@
 from flask import Flask, render_template, request
+from random import randint
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
+    return render_template('index.html')
+
+
+@app.route('/fortune', methods=['GET', 'POST'])
+def fortune():
     return render_template('fortune_form.html')
+
 
 @app.route('/fortune_results')
 def results():
+    name = request.args.get('name')
+    color = request.args.get('color')
+    hyd = request.args.get('hyd')
+    planet = request.args.get('planet')
+    if int(hyd) > 3:
+        prediction = "fantastic"
+    elif int(hyd) < 3:
+        prediction = "bad"
+    else:
+        prediction = "alright"
+
+    rand_color = "%06x" % randint(0, 0xFFFFFF)
+    lucky_number = randint(100000, 999999)
+
     fortune = {
-        'name': request.args.get('name'),
-        'siblings': request.args.get('siblings'),
-        'thing': request.args.get('thing')
+        'name': name,
+        'color': color,
+        'rand_color': rand_color,
+        'planet': planet,
+        'prediction': prediction,
+        'lucky_number': lucky_number
     }
+
     return render_template('results.html', fortune=fortune)
+
 
 if __name__ == '__main__':
     app.run()
